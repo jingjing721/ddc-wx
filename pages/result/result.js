e00e3959a57b21a789049d625d68bb7aa01c2aa2
严立晨 <lichen.yan@daydaycook.com>
2019-01-12 16:46:32 +0800

    canvas

--- pages/result/result.js
+++ pages/result/result.js
@@ -11,7 +11,7 @@ Page({
     resultBg: '',
 	  resultText: '',
 	  logo: '../../images/logo.png',
-	  close: '../../images/close.svg',
+	  close: '../../images/close.png',
 	  code: 'http://pic.qqtn.com/up/2018-1/2018012710125472621.jpg',
 	  resultArray: [],
   },
@@ -25,35 +25,92 @@ Page({
       resultBg: options.bg,
 	    resultText: options.blessText,
     })
+
+	  // let windowWidth = wx.getSystemInfoSync().windowWidth;
+	  // let userInfo = app.utils.getCache('userInfo');
+	  // const ctx = wx.createCanvasContext('canvasId');
+	  // ctx.drawImage(options.bg, 0, 0, 310, 300); // 绘制背景图
+	  // ctx.draw();
+	  // console.log(this.data.resultBg)
+	  // let that = this;
+	  // wx.getImageInfo({
+		 //  src: options.bg,//服务器返回的带参数的小程序码地址
+		 //  success: function (res) {
+			//   console.log(res, 'res');
+			//   //res.path是网络图片的本地地址
+			//
+			//   console.log('结束')
+		 //  },
+		 //  fail: function (res) {
+			//   //失败回调
+		 //  }
+	  // })
   },
 	/**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady () {
-		let windowWidth = wx.getSystemInfoSync().windowWidth;
-		let userInfo = app.utils.getCache('userInfo');
-		console.log(windowWidth);
+  	console.log(this.data.resultData);
 		const ctx = wx.createCanvasContext('canvasId');
-		ctx.drawImage(this.data.resultBg, this.remSize(16), this.remSize(30), this.remSize(310), 300) // 绘制背景图
-		ctx.drawImage(this.data.logo, this.remSize(80), this.remSize(8), this.remSize(18), this.remSize(18)) // 绘制logo
-		ctx.drawImage(this.data.close, this.remSize(106), this.remSize(12), this.remSize(10), this.remSize(10)) // 绘制close
-		ctx.setFontSize(14);
-		ctx.setFillStyle("#000");
-		ctx.fillText(`${userInfo.nickName}的新年餐桌`, this.remSize(128),this.remSize(22))
-		this.data.resultData.sort(this.sortNumber('zindex')) // 排序之后绘制层级
-		this.data.resultData.forEach((item) => { // 绘制 手动添加的菜品
-			ctx.drawImage(item.src, item.x, item.y, this.remSize(100), this.remSize(100));
-    })
-		ctx.fillText(this.data.resultText, 50, 280)
-		// ctx.drawImage(this.data.code, 0, 0, 120, 120) // 绘制背景
+		this.getImageInfo();
+		this.drawImg(ctx, img)
 		ctx.draw();
 
-		this.data.resultData.forEach((item) => {
-			 this.data.resultArray.push(item.id)
-		})
+		//ctx.drawImage(this.data.resultBg, this.remSize(16), this.remSize(30), this.remSize(310), this.remSize(300)); // 绘制背景图
 
-		console.log(this.data.resultArray.toString(), 'ctx');
+		// ctx.drawImage(this.data.logo, this.remSize(80), this.remSize(8), this.remSize(18), this.remSize(18)) // 绘制logo
+		// ctx.drawImage(this.data.close, this.remSize(106), this.remSize(12), this.remSize(10), this.remSize(10)) // 绘制close
+		// ctx.setFontSize(14);
+		// ctx.setFillStyle("#000");
+		// ctx.fillText(`${userInfo.nickName}的新年餐桌`, this.remSize(128),this.remSize(22))
+	//	this.data.resultData.sort(this.sortNumber('zindex')) // 排序之后绘制层级
+	// 	this.data.resultData.forEach((item) => { // 绘制 手动添加的菜品
+	// 		ctx.drawImage(item.src, item.x, item.y, this.remSize(100), this.remSize(100));
+   //  })
+		// ctx.fillText(this.data.resultText, 50, 280)
+		// ctx.drawImage(this.data.code, 0, 0, 120, 120) // 绘制背景
+
+
+		// this.data.resultData.forEach((item) => {
+		// 	 this.data.resultArray.push(item.id)
+		// })
+
+			//console.log(this.data.resultArray.toString(), 'ctx');
   },
+	/*
+	 * Description: 绘制图片
+	 * Types：img -> object || Array
+	 * Author: yanlichen <lichen.yan@daydaycook.com.cn>
+	 * Date: 2019/1/12
+	 */
+	drawImg(ctx, img) {
+		ctx.drawImage(img, this.remSize(16), this.remSize(30), this.remSize(310), this.remSize(300)); // 绘制背景图
+		ctx.drawImage(this.data.logo, this.remSize(80), this.remSize(8), this.remSize(18), this.remSize(18)) // 绘制logo
+		ctx.drawImage(this.data.close, this.remSize(106), this.remSize(12), this.remSize(10), this.remSize(10)) // 绘制close
+		this.data.resultData.forEach((item) => { // 绘制 手动添加的菜品
+			ctx.drawImage(item.src, item.x, item.y, this.remSize(100), this.remSize(100));
+		})
+	},
+	/*
+	 * Description: 对网络图片进行下载之后在绘制canvas
+	 * Author: yanlichen <lichen.yan@daydaycook.com.cn>
+	 * Date: 2019/1/12
+	 */
+	getImageInfo() {
+		this.data.resultData.forEach((item) => {
+			wx.getImageInfo({
+				src: item.src,//服务器返回的带参数的小程序码地址
+				success: function (res) {
+					return item.src = res.path;
+					//res.path是网络图片的本地地址
+				},
+				fail: function () {
+					app.utils.showToast('图片资源获取失败');
+				}
+			})
+		})
+	console.log(this.data.resultData, 'this.data.resultData')
+	},
 	/*
 	 * Description: 数组对象进行有小到大排序
 	 * Author: yanlichen <lichen.yan@daydaycook.com.cn>
